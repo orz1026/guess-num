2df484b0dbdf282866af11f1dea2a93efd85a030
alpha <orz1026@hotmail.com>
2021-01-12 10:43:11 +0800

    final version

--- guess-num.py
+++ guess-num.py
@@ -4,11 +4,15 @@
 # 猜錯的話 要告訴他 比答案大/小
 
 import random # random模組
-
-r = random.randint(1, 100) # random模組裡面的函式
-count = 0
+# 讓USER決定猜數字範圍8~11
+start = input('請決定隨機數字範圍開始值: ')
+end = input('請決定隨機數字範圍結束值: ')
+start = int(start)
+end = int(end)
+r = random.randint(start, end) # random模組裡面的函式
+count = 0 #記數器
 while True: # while True無限迴圈 要有break停止
-    count += 1 # count = count + 1
+    count += 1 #顯示一邊猜一邊顯示次數
     num = input('請猜數字: ') #input會強制存成字串
     num = int(num) #casting(型別轉換)
     if num == r:
@@ -18,6 +22,6 @@ while True: # while True無限迴圈 要有break停止
     elif num > r:
     	print('比答案大')
     elif num < r:
-    	print('比答案小')
-    print('你猜第', count, '次')
+    	print('比答案小') # 14~20行是一個if架構
+    print('你猜第', count, '次') #這樣可以不用寫三次在if架構裡面
 
